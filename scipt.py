925850743cfa2d5edc35aefb71a4d7807fd6a7ff
Woojgh <jamessalamonsen@gmail.com>
2017-07-15 16:01:51 -0700

    got scripts running

--- scipt.py
+++ scipt.py
@@ -6,4 +6,10 @@ from google.cloud import vision
 url = prompt('Give me a local image url or a web url to a image: ')
 
 if 'www.' not in url:
-    subprocess.call(['python', 'detect.py', 'document', url])
+    print("(choose from 'faces', 'labels', 'landmarks', 'text',  'logos', 'safe-search', 'properties', 'web', 'crophints', 'document')")
+    detect_choice = prompt('Please enter an identification type from list above: ')
+    subprocess.call(['python', 'detect.py', detect_choice, url])
+else:
+    print("(choose from 'faces-uri', 'labels-uri', 'landmarks-uri', 'text-uri', 'logos-uri', 'safe-search-uri', 'properties-uri', 'web-uri', 'crophints-uri', 'document-uri')")
+    detect_choice = prompt('Please enter an identification type from list above: ')
+    subprocess.call(['python', 'detect.py', detect_choice, url])

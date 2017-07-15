695aabd75f652858593eabecea28156ee010f9e9
Woojgh <jamessalamonsen@gmail.com>
2017-07-15 15:52:09 -0700

    got script working for vision ai file

--- scipt.py
+++ scipt.py
@@ -1,8 +1,9 @@
 from __future__ import unicode_literals
 import subprocess
 from prompt_toolkit import prompt
+from google.cloud import vision
 
-text = prompt('Give me some input: ')
-print('You said: %s' % text)
-if text:
-    subprocess.call(["ls", "-l"])
+url = prompt('Give me a local image url or a web url to a image: ')
+
+if 'www.' not in url:
+    subprocess.call(['python', 'detect.py', 'document', url])
